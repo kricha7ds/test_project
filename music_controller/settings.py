dcd0a2501b81cbe9cb02350616140b3d9b1911e1
Katherine Richards <kar3@ualberta.ca>
2021-03-14 17:39:47 -0600

    add .gitignore

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2az)4hkw1z=%ws*p(66wu5921^vka85mu$tpqu63fwc9b4go+%'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
