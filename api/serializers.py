369068d8316b38f2a55a9a59c8d47cca457f692b
Katherine Richards <kar3@ualberta.ca>
2021-03-14 12:32:06 -0600

    added create room post request

--- api/serializers.py
+++ api/serializers.py
@@ -8,3 +8,9 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room # define model to serialize
         fields = ('id', 'code', 'host', 'guest_can_pause', # define all fields we'd like to see in the serialization
                     'votes_to_skip', 'created_at') 
+
+# ensure that the data sent in the POST request is valid
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip') # defining fields that we want to be sent in POST request
--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .serializers import RoomSerializer
-from rest_framework import generics # create a class that inherits from a generic API view
+from .serializers import RoomSerializer, CreateRoomSerializer
+from rest_framework import generics, status # create a class that inherits from a generic API view
 from .models import Room
+from rest_framework .views import APIView
+from rest_framework.response import Response
+
 
 # API View - view list of all rooms
 # Can create a new Room or view all Rooms
@@ -9,3 +12,31 @@ class RoomView(generics.ListAPIView):
     queryset = Room.objects.all() # request all Rooms
     serializer_class = RoomSerializer # template to convert data to desired format
 
+# APIview allows us to override default CRUD methods
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer # optional for what we're doing
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+        
+        # serialize the data and give us a python representation so that we can check if it's valid
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            guest_can_pause = serializer.data.get('guest_can_pause')
+            votes_to_skip = serializer.data.get('votes_to_skip')
+            host = self.request.session.session_key
+
+            # if user already has session, simply update their preferences for the room
+            queryset = Room.objects.filter(host=host)
+            if queryset.exists():
+                room = queryset[0]
+                room.guest_can_pause = guest_can_pause
+                room.votes_to_skip = votes_to_skip
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
+            else: # create a new room
+                room = Room(host=host,guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room.save()
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+
+        return Response(RoomSerializer(room).data, status=status.HTTP_400_BAD_REQUEST)
