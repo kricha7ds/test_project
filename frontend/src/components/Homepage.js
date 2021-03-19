8dde430cac6d0fe8f0a51bb565b147ce3a752448
Katherine Richards <kar3@ualberta.ca>
2021-03-18 20:04:30 -0600

    added leave room functionality

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -11,6 +11,7 @@ export default class HomePage extends Component {
         this.state = {
             roomCode: null,
         };
+        this.clearRoomCode = this.clearRoomCode.bind(this);
     }
 
     async componentDidMount() {
@@ -45,6 +46,12 @@ export default class HomePage extends Component {
         );
     }
 
+    clearRoomCode() {
+        this.setState ({
+            roomCode: null,
+        });
+    }
+
     render() {
         return (
         <Router>
@@ -62,7 +69,12 @@ export default class HomePage extends Component {
                 />
                 <Route path='/join' component={ RoomJoinPage } />
                 <Route path='/create' component={ CreateRoomPage } />
-                <Route path='/room/:roomCode' component={ Room } />
+                <Route 
+                    path='/room/:roomCode' 
+                    render={(props) => {
+                        return <Room {...props} leaveRoomCallback={this.clearRoomCode} />;
+                    }} 
+                />
             </Switch>
         </Router>
         );
